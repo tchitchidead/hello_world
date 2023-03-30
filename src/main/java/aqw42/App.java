cf051fb61930f5608c2a05a4b02fec7d3110c14b
Debian <debian@debian10.linuxvmimages.local>
2023-03-30 09:48:27 -0500

    broken again

--- src/main/java/aqw42/App.java
+++ src/main/java/aqw42/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static String get_message()
     {
-        return "Hello Worlo !";
+        return "Hello World !";
     }
 
     public static void main( String[] args )
