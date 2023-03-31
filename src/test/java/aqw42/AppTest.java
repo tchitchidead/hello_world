c8e2b149526df449b576076d0d5fe1dceb517459
Corentin Noel <25183686+aqw42@users.noreply.github.com>
2023-03-31 14:33:51 +0200

    Update AppTest.java

--- src/test/java/aqw42/AppTest.java
+++ src/test/java/aqw42/AppTest.java
@@ -17,6 +17,6 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertEquals( "The message is not right", app.get_message(), "Hello World !");
+        assertEquals( "The message is not right", app.get_message(), "Hello Wrld !");
     }
 }
